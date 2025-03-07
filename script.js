113be31fc92e46dbd91fc1a1790e27dc98625203
Mouli <128670409+Moulithar@users.noreply.github.com>
2025-03-07 23:57:14 +0530

    printforecast problem solved

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Print forecast");
+const array = [2, 6, 5, 2, 3];
+
+function printForecast(array) {
+  let string = "";
+  for (let i = 0; i < array.length; i++) {
+    string = string + `...${array[i]} @ celsius in ${i + 1} days`;
+  }
+  return string;
+}
+
+console.log(printForecast(array));
