936e039c50e89a0f3682857398d3724205edbe50
Moulithar Manoharan <moulipri@gmail.com>
2025-08-13 09:23:28 +0530

    feat: add flight display functionality and update UI styling with gradient backgrounds

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -30,25 +30,49 @@ console.log(maskedNumber);
 //   calculate_AGE
 // delayed_departure
 
-const textArea = document.createElement("textarea");
-const button = document.createElement("button")
+const textArea = document.createElement('textarea');
+const button = document.createElement('button');
 
 document.body.appendChild(textArea);
 document.body.appendChild(button);
 
-button.textContent = 'Sumbit';
+button.textContent = 'Submit';
 
-const handleClick =()=>{
-  const value = textArea.value;
-  const formattedArray = value.replaceAll("// ", "").split("\n");
+const handleClick = value => {
+  const formattedArray = value.replaceAll('// ', '').split('\n');
 
-  for(const [index,row] of formattedArray.entries()){
-    const [firstWord, lastWord] = row.toLowerCase().trim().split("_");
-    const camelCase = firstWord + lastWord.replace(lastWord[0], lastWord[0].toUpperCase());
-    console.log(camelCase.padEnd(20," ")+"#".repeat(index+1))
-
+  for (const [index, row] of formattedArray.entries()) {
+    const [firstWord, lastWord] = row.toLowerCase().trim().split('_');
+    const camelCase =
+      firstWord + lastWord.replace(lastWord[0], lastWord[0].toUpperCase());
+    console.log(camelCase.padEnd(20, ' ') + '#'.repeat(index + 1));
   }
+};
 
-}
+button.addEventListener('click', () => handleClick(textArea.value));
+button.addEventListener('click', handleClick);
 
-button.addEventListener("click", handleClick)
+const flights =
+  '+Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// # Delayed Departure from FAO to TXL (11h25)
+// #           Arrival from BRU to FAO (11h45)
+// #   Delayed Arrival from HEL to FAO (12h05)
+// #         Departure from FAO to LIS (12h30)
+
+const displayFlight = flights => {
+  const splittedFlights = flights.slice(1).split('+');
+
+  for (const row of splittedFlights) {
+    const [type, from, to, time] = row.split(';');
+    const message = `${type.replaceAll('_', ' ').trim()} from ${from
+      .slice(0, 3)
+      .toUpperCase()} to ${to.slice(0, 3).toUpperCase()} (${time.replace(
+      ':',
+      'h'
+    )})`;
+    console.log('#' + message.padStart(50, ' '));
+  }
+};
+
+displayFlight(flights);
