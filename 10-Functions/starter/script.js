674bd8ebb1320bb3e2a526ab03398a22d8227150
Moulithar Manoharan <moulipri@gmail.com>
2025-08-17 16:44:52 +0530

    feat: initialize Bankist app with account management and UI components

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-const greet = () => alert('Hello');
+// const greet = () => alert('Hello');
 
 // const btnElement = document.querySelector('.buy');
 
@@ -76,3 +76,142 @@ const displayFlight = flights => {
 };
 
 displayFlight(flights);
+
+const greet = greeting => name => console.log(`${greeting}, -> ${name}`);
+
+greet('hey')('moogi manga');
+
+const lufthansa = {
+  name: 'lufthansa airlines',
+  iataCode: 'LH',
+  bookings: [],
+  book(name, iataCode) {
+    console.log(`${this.name} booked a seat on ${this.iataCode}`);
+    this.bookings.push({ name, iataCode });
+  },
+};
+
+lufthansa.book('Moulithar', 'LH123');
+
+const euroWings = {
+  name: 'eurowings airlines',
+  iataCode: 'EW',
+  bookings: [],
+};
+const swissAirlines = {
+  name: 'swiss airlines',
+  iataCode: 'SW',
+  bookings: [],
+};
+
+const bookFn = lufthansa.book;
+
+bookFn.call(euroWings, 'Manga', 'EW');
+bookFn.call(swissAirlines, ' ', 'SW');
+
+const addTaxRate = value => {
+  return rate => {
+    console.log(value + value * rate);
+  };
+};
+
+const addVAT2 = addTaxRate(200);
+
+addVAT2(0.25);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the 
+  value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number 
+  makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an 
+input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the 
+results array as it is, using console.log(). This should be the default option. If type is 'string', 
+display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and 
+the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const answerPollBtn = document.querySelector('.poll');
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: [
+    { option: 'JavaScript', value: 0 },
+    { option: 'Python', value: 0 },
+    { option: 'Rust', value: 0 },
+    { option: 'C++', value: 0 },
+  ],
+  displayResults(type = 'array') {
+    if (type == 'string') {
+      console.log(`Poll results are ${this.options.map((opt)=> opt.value).join(", ")}`);
+    } else if (type == 'array') {
+      console.log(this.options);
+    }
+  },
+  registerNewAnswer() {
+    const answer = prompt(
+      `${this.question} \n${this.options
+        .map((opt, index) => `${index}: ${opt.option}\n`)
+        .join('')} (Write option number)`
+    );
+    answer >= 0 && answer < this.options.length
+      ? this.options[answer].value++
+      : console.log('please enter a valid option');
+    // this.displayResults();
+    this.displayResults();
+  },
+
+};
+
+const registerNewAnswer = poll.registerNewAnswer.bind(poll);
+
+answerPollBtn.addEventListener('click', registerNewAnswer);
+
+
+let f;
+const g = ()=>{
+
+  const a = 63;
+   f = function(){
+    console.log(a*2)
+  }
+}
+
+g();
+f();
+
+(function(){
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+  document.body.addEventListener("click", ()=>{
+    header.style.color = "blue"
+  })
+})()
